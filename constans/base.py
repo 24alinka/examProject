030ee8995e0faafbaf5c9c604f860f7be0e4006f
Alina <alinagritsan77@gmail.com>
2022-10-06 19:38:29 +0300

    2022-10-06

---
+++ constans/base.py
@@ -0,0 +1,2 @@
+DRIVER_PATH = "/Users/User/PycharmProjects/pythonProject1/chromedriver"
+BASE_URL = "http://contentjv6.perfect-sitebank.com/"
---
+++ test_start_page.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+
+class TestStartPage:
+    def test_sign_up(self):
+        driver = webdriver.Chrome("C:/Users/User/PycharmProjects/examProject/chromedriver.exe")
